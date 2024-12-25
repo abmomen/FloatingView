29641b049bbe453bdbcc910dc31e1822bc6ec957
Abdul Momen <abdulmomen130@gmail.com>
2024-12-25 20:00:25 +0600

    Initial Commit

---
+++ FloatingDemo/FloatingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingDemoApp.swift
+//  FloatingDemo
+//
+//  Created by Abdul Momen on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
